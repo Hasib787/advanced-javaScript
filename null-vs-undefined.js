f142bcee0d5dd0e2099150e647ae0acbb5761dd3
Hasib787 <mr.hasib45@gmail.com>
2021-02-03 01:53:38 +0600

    null vs undefined for interview cleard

---
+++ null-vs-undefined.js
@@ -0,0 +1,26 @@
+null
+let a = null;
+console.log(a);
+
+undefined
+let name;
+console.log(name);
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+}
+const result = add(5, 6); // no return value (undefined)
+console.log(result);
+
+function add(num1, num2) {
+    console.log(num1, num2);
+}
+const result = add(5);
+console.log(result);
+
+const premik = {name:"smart dude", phone:45787554};
+console.log(premik.gf);
+
+let fun=undefined;
+console.log(fun);
+
