0b85a0f77c73d5875e6721630f0bbccd1bcb7180
Hasib787 <mr.hasib45@gmail.com>
2021-02-03 01:15:48 +0600

    ture vs false

---
+++ true-false.js
@@ -0,0 +1,16 @@
+//falsy condition
+//false
+//0
+//""
+//undefine
+//NaN
+
+//Truthy Condition
+//'0', ' ', [], {} 
+let age=0;
+
+if(age || age==0){
+    console.log("condition is true");
+}else{
+    console.log('Condition is false');
+}
