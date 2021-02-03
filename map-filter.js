640fb12b5742cebf321eb13333a91b7e2d7c8e51
Hasib787 <mr.hasib45@gmail.com>
2021-02-03 15:38:37 +0600

    final

--- map-filter.js
+++ map-filter.js
@@ -1,22 +1,22 @@
 const numbers = [8, 6, 7, 9, 15, 14, 5];
-// const output = [];
-// for (let i = 0; i < numbers.length; i++) {
-//     const element = numbers[i];
-//     const result = element * element;
-//     output.push(result);
-// }
-// console.log(output);
+const output = [];
+for (let i = 0; i < numbers.length; i++) {
+    const element = numbers[i];
+    const result = element * element;
+    output.push(result);
+}
+console.log(output);
 
-// function square(element) {
-//     return element * element;
-// }
+function square(element) {
+    return element * element;
+}
 
-// const square = element => element * element;
-// const square = x => x * x;
+const square = element => element * element;
+const square = x => x * x;
 
-// const result = numbers.map(function square(element) {
-//     return element * element;
-// });
+const result = numbers.map(function square(element) {
+    return element * element;
+});
 
 const result = numbers.map(x => x * x);
 
